6ebabcbeb84285724fec112a466c290d0b8c08e3
Clive Nyaga <nyagakagotho@gmail.com>
2025-11-19 07:01:11 +0300

    Adds dynamic URLS and a GET only route

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/home')
-def home():
-    return 'Welcome to the home page!'
+@app.route('/home/users/<string:username>/posts/<int:id>')
+def home(username, id):
+    return 'Welcome to the home page ' + username + '!' + ' Your ID is ' + str(id) + '.'
+
+@app.route('/get', methods=['GET'])
+def get_route():
+    return 'This is a GET route. 200 OK!'
 
 if __name__ == '__main__':
     app.run(debug=True)
