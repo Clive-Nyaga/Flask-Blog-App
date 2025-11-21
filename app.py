c15c8ec4cbd8dbff3ba2b5be415c00c10c204739
Clive Nyaga <nyagakagotho@gmail.com>
2025-11-21 16:01:24 +0300

    Creates a database for posts

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(50), nullable=True, default='Unknown Author')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        # return f"Blog Post('{self.title}', '{self.author}')"
+        return 'Blog Post ' + str(self.id)
 
 all_posts = [
     {
